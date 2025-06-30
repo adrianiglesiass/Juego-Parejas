3bfde31b96165b7b3cc1cb2bb3f53ae05c39d0f3
Adrián Iglesias <adrianiglesias010@gmail.com>
2025-06-30 17:24:25 +0200

    feat: Logica completa del juego

--- script.js
+++ script.js
@@ -4,10 +4,17 @@ let tarjeta2 = null;
 let valor1 = null;
 let valor2 = null;
 let aciertos = 0;
+let movimientos = 0;
 let temporizador = false;
 let timer = 50;
 let timerInicial = 50;
 let tiempoRegresivoId = null;
+
+let winAudio = new Audio("./sounds/win.wav");
+let loseAudio = new Audio("./sounds/lose.wav");
+let clickAudio = new Audio("./sounds/click.wav");
+let rightAudio = new Audio("./sounds/right.wav");
+let wrongAudio = new Audio("./sounds/wrong.wav");
 
 let mostrarAciertos = document.getElementById("aciertos");
 let tiempoRestante = document.getElementById("t-rest");
@@ -18,21 +25,6 @@ numeros = numeros.sort(() => {
 	return Math.random() - 0.5;
 });
 
-function destapar(id) {
-	const boton = document.getElementById(id);
-	boton.innerText = numeros[id];
-	boton.disabled = true;
-	tarjetasDestapadas++;
-
-	if (tarjetasDestapadas === 1) {
-		tarjeta1 = boton;
-		valor1 = numeros[id];
-	} else if (tarjetasDestapadas === 2) {
-		tarjeta2 = boton;
-		valor2 = numeros[id];
-	}
-}
-
 function contarTiempo() {
 	tiempoRegresivoId = setInterval(() => {
 		timer--;
@@ -40,8 +32,61 @@ function contarTiempo() {
 		if (timer == 0) {
 			clearInterval(tiempoRegresivoId);
 			bloquearTarjetas();
+			loseAudio.play();
 		}
-	});
+	}, 1000);
 }
 
-function bloquearTarjetas() {}
+function bloquearTarjetas() {
+	for (let i = 0; i <= 15; i++) {
+		let tarjetaBloqueada = document.getElementById(i);
+		tarjetaBloqueada.innerHTML = `<img src="./images/${numeros[i].png}"`;
+		tarjetaBloqueada.disabled = true;
+	}
+}
+
+function destapar(id) {
+	if (temporizador == false) {
+		contarTiempo();
+		temporizador = true;
+	}
+	tarjetasDestapadas++;
+
+	if (tarjetasDestapadas == 1) {
+		tarjeta1 = document.getElementById(id);
+		valor1 = numeros[id];
+		tarjeta1.innerHTML = `<img src="./images/${valor1}.png">`;
+		clickAudio.play();
+		tarjeta1.disabled = true;
+	} else if (tarjetasDestapadas == 2) {
+		tarjeta2 = document.getElementById(id);
+		valor2 = numeros[id];
+		tarjeta2.innerHTML = `<img src="./images/${valor2}.png">`;
+		tarjeta2.disabled = true;
+		movimientos++;
+		mostrarMovimientos.innerHTML = `Movimientos: ${movimientos}`;
+
+		if (valor1 == valor2) {
+			tarjetasDestapadas = 0;
+			aciertos++;
+			mostrarAciertos.innerHTML = `Aciertos: ${aciertos}`;
+
+			if (aciertos == 8) {
+				clearInterval(tiempoRegresivoId);
+				mostrarAciertos.innerHTML = `Aciertos: ${aciertos}`;
+				mostrarMovimientos.innerHTML = `Movimientos: ${movimientos}`;
+				tiempoRestante.innerHTML = `Fantastico: ${timerInicial - timer} segundos`;
+				winAudio.play();
+			}
+		} else {
+			setTimeout(() => {
+				tarjeta1.innerHTML = "";
+				tarjeta2.innerHTML = "";
+				tarjeta1.disabled = false;
+				tarjeta2.disabled = false;
+				tarjetasDestapadas = 0;
+				wrongAudio.play();
+			}, 700);
+		}
+	}
+}
